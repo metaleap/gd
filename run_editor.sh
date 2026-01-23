0459bf55419639d90962e0421bf00f28d1168d61
Phil Schumann <metaleap@users.noreply.github.com>
2026-01-23 23:12:09 +0100

    ..

--- run_editor.sh
+++ run_editor.sh
@@ -5,7 +5,6 @@ thisScriptsFilePath="$(readlink --canonicalize-existing "$0")"
 thisScriptsDirPath="$(dirname "$thisScriptsFilePath")"
 cd $thisScriptsDirPath
 
-# cd 3rdparty/turanszkij_WickedEngine/.build_dbg/Editor && AMD_VULKAN_ICD=RADV DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1 ./Editor
 cd 3rdparty/turanszkij_WickedEngine/.build_release_gcc/Editor && AMD_VULKAN_ICD=RADV DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1 ./Editor
 cd $thisScriptsDirPath
 cp 3rdparty/turanszkij_WickedEngine/.build_release_gcc/Editor/config.ini 3rdparty/
