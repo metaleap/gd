632a1f1d0f44725875b6fd6d4ffaee04fe9651b6
Phil Schumann <metaleap@users.noreply.github.com>
2026-01-31 11:54:51 +0100

    ..

--- 3rdparty/startup.lua
+++ 3rdparty/startup.lua
@@ -1,3 +1,15 @@
 SetProfilerEnabled(false)
 --physics.SetFrameRate(120)
 --physics.SetAccuracy(4)
+
+runProcess(function()
+	while true do
+		update()
+		if
+			input.Press(string.byte("Q"))
+			and (input.Press(KEYBOARD_BUTTON_LCONTROL) or input.Press(KEYBOARD_BUTTON_RCONTROL))
+		then
+			application.Exit()
+		end
+	end
+end)
